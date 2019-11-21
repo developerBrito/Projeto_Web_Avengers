e639685c5a5797904db9c6c730ad9537b98e724f
Kleber Brito <kleber.brito@etec.sp.gov.br>
2019-11-21 09:09:01 -0300

    Atualização repositório do Beto

---
+++ Projeto Avangers/js/funcoes.js
@@ -0,0 +1,23 @@
+function botaocap() {
+       document.getElementById("bd").style.background = "blue";
+}
+
+function botaoiron() {
+       document.getElementById("bd").style.background = "#ff0000";
+}
+
+function botaothor() {
+       document.getElementById("bd").style.background = "#a5a5a5";
+}
+
+function botaohulk() {
+       document.getElementById("bd").style.background = "#037500";
+}
+
+function botaoviuva() {
+       document.getElementById("bd").style.background = "#C7620A";
+}
+
+function botaogaviao() {
+       document.getElementById("bd").style.background = "#8310A3";
+}
